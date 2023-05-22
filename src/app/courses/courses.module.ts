1068f42c54bdd0e74653980fdee4c2fd0cdee119
wallace <wallace_wolv@hotmail.com>
2023-05-22 16:01:43 -0300

    feat: Criando o Módulo de Cursos e Usando Roteamento com Lazy Loading

---
+++ src/app/courses/courses.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { CoursesRoutingModule } from './courses-routing.module';
+import { CoursesComponent } from './courses.component';
+
+
+@NgModule({
+  declarations: [
+  
+    CoursesComponent
+  ],
+  imports: [
+    CommonModule,
+    CoursesRoutingModule
+  ]
+})
+export class CoursesModule { }
