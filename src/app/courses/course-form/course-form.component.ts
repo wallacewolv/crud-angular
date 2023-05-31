8bd3beecdd90543498121199e25d5ddb5beae52d
wallace <wallace_wolv@hotmail.com>
2023-05-31 03:48:32 -0300

    feat: Formulário: Salvando os dados com HTTP POST + tratamento de erros

--- src/app/courses/course-form/course-form.component.ts
+++ src/app/courses/course-form/course-form.component.ts
@@ -1,5 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
+import { CoursesService } from '../services/courses.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
+
+interface CourseCategory {
+  value: string;
+  viewValue: string;
+}
 
 @Component({
   selector: 'app-course-form',
@@ -8,17 +15,44 @@ import { FormBuilder, FormGroup } from '@angular/forms';
 })
 export class CourseFormComponent implements OnInit {
   form: FormGroup;
+  courseCategories: Array<CourseCategory>;
 
-  constructor(private formBuilder: FormBuilder) {
+  constructor(
+    private formBuilder: FormBuilder,
+    private coursesService: CoursesService,
+    private snackBar: MatSnackBar
+  ) {
     this.form = this.formBuilder.group({
       name: [null],
       category: [null],
     });
+
+    this.courseCategories = [
+      { value: 'null', viewValue: '' },
+      { value: 'front-end', viewValue: 'Front-End' },
+      { value: 'back-end', viewValue: 'Back-End' },
+      { value: 'big-data', viewValue: 'Big Data' },
+    ];
   }
 
   ngOnInit(): void {}
 
-  onSubmit() {}
+  onSubmit() {
+    this.coursesService.save(this.form.value).subscribe(
+      (result) => {
+        console.log(result);
+      },
+      (error) => {
+        this.onError();
+      }
+    );
+  }
 
   onCancel() {}
+
+  private onError() {
+    this.snackBar.open('Erro ao salvar curso', 'X', {
+      duration: 5000,
+    });
+  }
 }
--- src/app/courses/services/courses.service.ts
+++ src/app/courses/services/courses.service.ts
@@ -19,4 +19,8 @@ export class CoursesService {
       tap((courses) => console.log(courses))
     );
   }
+
+  save(record: Course): Observable<Course> {
+    return this.httpClient.post<Course>(this.API, record).pipe(first());
+  }
 }
