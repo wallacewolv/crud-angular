baf9fee570a76769a40db8b1bd15b112abdceef5
Wallace W <wallace_wolv@hotmail.com>
2025-05-24 17:25:03 -0300

    feat: 06 - Criando Material Table para Listar Cursos

--- src/app/courses/courses.component.ts
+++ src/app/courses/courses.component.ts
@@ -1,12 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MatTableModule } from '@angular/material/table';
+
+import { Course } from './model/course';
 
 @Component({
   selector: 'app-courses',
   standalone: true,
-  imports: [],
+  imports: [MatTableModule],
   templateUrl: './courses.component.html',
-  styleUrl: './courses.component.scss'
+  styleUrl: './courses.component.scss',
 })
-export class CoursesComponent {
+export class CoursesComponent implements OnInit {
+  courses: Course[] = [
+    {
+      _id: '1',
+      name: 'Angular',
+      category: 'front-end',
+    },
+    {
+      _id: '2',
+      name: 'Node.js',
+      category: 'back-end',
+    },
+  ];
 
+  displayedColumns = ['name', 'category'];
+
+  constructor() {}
+
+  ngOnInit(): void {}
 }
