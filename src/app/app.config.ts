978c7084b860dedf74bf9706c976105e4cd8ac47
Wallace W <wallace_wolv@hotmail.com>
2025-05-24 15:07:48 -0300

    initial commit

---
+++ src/app/app.config.ts
@@ -0,0 +1,20 @@
+import { provideHttpClient } from '@angular/common/http';
+import {
+  ApplicationConfig,
+  importProvidersFrom,
+  provideZoneChangeDetection,
+} from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { provideRouter } from '@angular/router';
+
+import { routes } from './app.routes';
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideHttpClient(),
+    importProvidersFrom(BrowserAnimationsModule, FormsModule),
+  ],
+};
