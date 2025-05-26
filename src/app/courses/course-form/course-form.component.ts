52e543c3f4f770769048a2a7f07624d6132bdece
Wallace W <wallace_wolv@hotmail.com>
2025-05-26 00:18:02 -0300

    feat: 19: Formulário para Criar Cursos

--- src/app/courses/course-form/course-form.component.ts
+++ src/app/courses/course-form/course-form.component.ts
@@ -1,12 +1,34 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import {
+  ReactiveFormsModule,
+  UntypedFormBuilder,
+  UntypedFormControl,
+  UntypedFormGroup,
+} from '@angular/forms';
 
 import { AppMaterialModule } from '../../shared/app-material/app-material.module';
 
 @Component({
   selector: 'app-course-form',
   standalone: true,
-  imports: [AppMaterialModule],
+  imports: [CommonModule, ReactiveFormsModule, AppMaterialModule],
   templateUrl: './course-form.component.html',
   styleUrl: './course-form.component.scss',
 })
-export class CourseFormComponent {}
+export class CourseFormComponent implements OnInit {
+  courseForm: UntypedFormGroup;
+
+  constructor(private formBuilder: UntypedFormBuilder) {
+    this.courseForm = this.formBuilder.group({
+      name: new UntypedFormControl(null),
+      category: new UntypedFormControl(null),
+    });
+  }
+
+  ngOnInit(): void {}
+
+  onSubmit() {}
+
+  onCancel() {}
+}
